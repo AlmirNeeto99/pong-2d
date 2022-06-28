6bb487d8914e60c32b97d7d3e6fd00bf6486f0b0
Almir Neto <almirneto338@gmail.com>
2022-06-28 00:44:37 -0300

    - Adding headers - Adding update function - Adding direction vector

--- src/Ball.hpp
+++ src/Ball.hpp
@@ -3,14 +3,18 @@
 #define BALL_H
 
 #include "SFML/Graphics.hpp"
+#include "Window.hpp"
+#include "cmath"
 
 class Ball : public sf::CircleShape {
    public:
     Ball(int, int);
     int x, y;
+    void update(double);
 
    private:
     static constexpr int RADIUS = 10;
+    sf::Vector2f direction;
 };
 
 #endif
