11e6b936ccb1181319b1152486796dbe3f83d32a
Almir Neto <almirneto338@gmail.com>
2022-06-27 21:14:12 -0300

    Adding update functions

--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -3,3 +3,20 @@
 Paddle::Paddle(int x, int y) : RectangleShape(sf::Vector2f(this->WIDTH, this->HEIGHT)) {
     this->setPosition(sf::Vector2f(x, y));
 }
+
+void Paddle::update(double dt) {
+    int speed = 50;
+    sf::Vector2f curPos = this->getPosition();
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+        curPos.y -= (1.f * dt) * speed;
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+        curPos.y += (1.f * dt) * speed;
+    }
+    if (curPos.y <= 0) {
+        curPos.y = 0;
+    } else if ((curPos.y + this->HEIGHT) >= Window::HEIGHT) {
+        curPos.y = Window::HEIGHT - this->HEIGHT;
+    }
+    this->setPosition(curPos);
+}
